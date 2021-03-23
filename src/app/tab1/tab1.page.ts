bf937ea8b24f13d34409651a5a66db2175014a7f
Darwin Uzcategui Portati Linux <darwin.uzcategui1973@gmail.com>
2021-03-23 18:46:02 -0400

    Fin seccion1

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(){
+
+    let num = 23;
+    // debugger;
+
+    num = 20;
+
+    num = num +10;
+
+    num =0;
+
+    console.log(num);
+
+  }
+
+}
